f4bb4d321c50c9d67d9a368a7711d2528537b088
jojocreampuff <randal.engelhardt.205@my.csun.edu>
2024-07-09 19:12:01 -0700

    first time working on work station

--- Infinite_SNAC_2023/z_testing_files/convert_data.m
+++ Infinite_SNAC_2023/z_testing_files/convert_data.m
@@ -1,15 +1,22 @@
-clear; clc; close;
 
-data_thing = load('trained_weights.mat','Position_W', "Attitude_W")
+
+% data_thing = load('trained_weights.mat','Position_W', "Attitude_W")
 
 % Extract and convert the matrices to double
-Position_W = single(data_thing.Position_W);
-Attitude_W = single(data_thing.Attitude_W);
+
+% Position_W = single(data_thing.Position_W);
+% Attitude_W = single(data_thing.Attitude_W);
+
+Position_W = single(Position_W);
+Attitude_W = single(Attitude_W);
 
 save("trained_weights","Attitude_W","Position_W","-v7.3")
 
+
+
+save("z_reference","r_smooth","r_initial","-v7.3")
 data = load("z_reference.mat", "r_smooth");
 
-ref_states = single(data.r_smooth.r_smooth_1);
+ref_states = single(r_smooth.r_smooth_1);
 
 save("reference_SITL","ref_states","-v7.3")
